9a0d1a450388bf0c7d38c68df62e9c9cd73f31e1
unknown <you@example.com>
2020-08-06 01:57:15 -0300

    Updates

---
+++ precache-manifest.bf7f8e12743f7e0997c6f60997d207a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe6329063cecef74a5908d28067a4fc",
+    "url": "/teste-api/index.html"
+  },
+  {
+    "revision": "cc5d33ab180ed73f93fc",
+    "url": "/teste-api/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "47cf00936e24ea946a52",
+    "url": "/teste-api/static/css/main.4dc82b3d.chunk.css"
+  },
+  {
+    "revision": "cc5d33ab180ed73f93fc",
+    "url": "/teste-api/static/js/2.78df7fac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/teste-api/static/js/2.78df7fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47cf00936e24ea946a52",
+    "url": "/teste-api/static/js/main.72883a30.chunk.js"
+  },
+  {
+    "revision": "d13f9927093bc2e56552",
+    "url": "/teste-api/static/js/runtime-main.c39127a2.js"
+  }
+]);
